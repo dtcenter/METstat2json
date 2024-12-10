d7de012cdaa60aa8c084587807a542221e28878f
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2024-12-10 15:08:06 -0700

    Update test to match the new line type name

--- pkg/structColumnDefs/definitions_test.go
+++ pkg/structColumnDefs/definitions_test.go
@@ -9,7 +9,7 @@ func TestParseIt(t *testing.T) {
 		expected string
 	}{
 		{
-			lineType: "SL1L2",
+			lineType: "STAT_SL1L2_V12_0",
 			line:     "V12.0.0 FCST  NA   120000    20050807_120000 20050807_120000 000000   20120409_113000 20120409_123000 FCST     None       Surface  TMP     NA        Z2      ADPSFC FULL    NEAREST     1           NA          NA         NA         NA    SL1L2      4511 59.12348 279.18203 16572.91166 14380.0603  77998.04015 220.05855",
 			expected: "parsed",
 		},
